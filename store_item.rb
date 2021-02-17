3068eb37a05dcf18a0349e0be0545fc0c4f016b6
Tyler Epstein <tylerepstein27@gmail.com>
2021-02-17 13:30:42 -0500

    adds three items(mochi, green tea, rice)

---
+++ store_item.rb
@@ -0,0 +1,10 @@
+=begin
+Use hashes with symbols to represent the following scenario:
+ You are the owner of a store that sells items (you decide what specifically). Each item has various properties such as color, price, etc.
+Represent 3 items using hashes. Each hash should have the same keys with different values.
+Be sure to use symbols for the keys. Try creating hashes using both types of hash symbol syntaxes. (Ruby syntax {:a => 123} vs. “JavaScript” syntax {a: 123}).
+=end
+
+mochi = { section: "frozen", size: "12 pieces", price: 5.50 }
+green_tea = { section: "refrigerated", size: "16 oz", price: 2 }
+rice = { section: "grocery", size: "10 pound bag", price: 30 }
