d7ed60442c8649c29915d355109c0ebb2d88f235
Tyler Epstein <tylerepstein27@gmail.com>
2021-02-18 14:01:52 -0500

    adds Perishable class which inherits from Items

--- store_item.rb
+++ store_item.rb
@@ -37,6 +37,20 @@ class Item
   end
 end
 
+class Perishable < Item
+  attr_reader :shelf_life
+
+  def initialize(input_options)
+    super
+    @shelf_life = input_options[:shelf_life]
+  end
+end
+
 mochi = Item.new(section: "frozen", size: "12 pieces", price: 5.50, in_stock: true)
 green_tea = Item.new(section: "refrigerated", size: "16 oz", price: 1.50, in_stock: true)
 rice = Item.new(section: "grocery", size: "10 pounds", price: 30, in_stock: true)
+
+milk = Perishable.new(section: "refrigerated", size: "1 gal", price: 4, in_stock: true, shelf_life: "12 days")
+
+puts milk.section
+puts milk.shelf_life
