bec15fb4dddb75ca51b87b29ac4f255b89d6f2d9
Tyler Epstein <tylerepstein27@gmail.com>
2021-02-18 13:38:31 -0500

    adds 'super' to initialize method to help keep things DRY

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -18,12 +18,27 @@ class Vehicle
 end
 
 class Car < Vehicle
+  attr_reader :make, :model, :fuel
+
+  def initialize
+    super
+    @make = "Pontiac"
+    @model = "Firebird"
+    @fuel = "Premium"
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Vehicle
+  def initialize
+    super
+    @type = "Mountain Bike"
+    @weight = "20 pounds"
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
@@ -43,3 +58,7 @@ puts bike.turn("South")
 
 car.honk_horn
 bike.ring_bell
+
+puts car.make
+puts car.model
+puts car.fuel
